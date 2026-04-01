005be2fcb2491f74a69afbcc288398d9d3a137e9
KristofferHoff <kristoffer.hoff@me.com>
2026-04-01 12:38:06 +0200

    Jeg har laget 1 spørsmål, som en mal til andre spørsmål som skal settes opp!
    så bare å fortsette på malen så har vi flere spørsmål

--- UI_app/src/App.jsx
+++ UI_app/src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import QuestionCard from './components/questions/QuestionCard'
 
 function App() {
   return (
     <>
-     <h1>Hello World</h1>
+    <h1>Hello World</h1>
+      <QuestionCard />
     </>
   )
 }
--- UI_app/src/components/questions/QuestionCard.jsx
+++ UI_app/src/components/questions/QuestionCard.jsx
@@ -1,1 +1,27 @@
-export default function QuestionCard() {}
+import { useState } from "react"
+import MultipleChoice from "./MultipleChoice"
+
+const testSpørsmål = {
+  question: "Hva er en array i programmering?",
+  options: [
+    { id: "A", text: "En enkelt variabel som lagrer én verdi" },
+    { id: "B", text: "En liste som kan lagre flere verdier i én variabel" },
+    { id: "C", text: "En funksjon som returnerer tall" },
+    { id: "D", text: "Et nøkkelord for å lage løkker" },
+  ],
+  correct: "B",
+}
+
+export default function QuestionCard() {
+  const [valgtSvar, setValgtSvar] = useState(null)
+
+  return (
+    <div>
+      <MultipleChoice
+        question={testSpørsmål}
+        valgtSvar={valgtSvar}
+        onSvar={setValgtSvar}
+      />
+    </div>
+  )
+}
