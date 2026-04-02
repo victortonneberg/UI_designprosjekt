4a97e537abe89cde8a41c667ab99115669157396
nikolaiols <nikolaio@hiof.no>
2026-04-02 14:51:34 +0200

    lagt inn Playerinfo og gjort det mulig å velge navn og vansklighetsgrad
    vi må rendre ut antall "player info kort" basert på hvor mange spillere som er valgt, så må dette lagres et sted. per nå tar den kun å gjør det mulig å skrive inn navn og vanskelighetsgrad. fjernet også router da dette ga feil i testingen

--- UI_app/src/App.jsx
+++ UI_app/src/App.jsx
@@ -1,16 +1,17 @@
+import PlayerInfo from './components/game/PlayerInfo';
 import Layout from './components/Layout'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+   
       <Layout>
         <Routes>
           {/* <Route path="/" element={<Home />} />  ikke det vi skal ha */}
           {/* <Route path="/about" element={<About />} /> */}
         </Routes>
       </Layout>
-    </Router>
+    
   );
 }
 export default App
--- UI_app/src/main.jsx
+++ UI_app/src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,5 +7,5 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
 )
