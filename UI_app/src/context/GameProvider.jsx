79fc1233beec0eaaa2713a22511cd9a3f7e5458d
Victor Tønneberg <victon2003@gmail.com>
2026-04-07 17:34:33 +0200

    Bossfight
    Lagt til context for bossfights

--- UI_app/src/context/GameProvider.jsx
+++ UI_app/src/context/GameProvider.jsx
@@ -2,29 +2,44 @@ import { useState } from "react";
 import { GameContext } from "./GameContext";
 
 export const GameProvider = ({ children }) => {
-    const [players, setPlayers] = useState([]);
-    const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
-    const [phase, setPhase] = useState("roll-d20");
+  const [players, setPlayers] = useState([]);
+  const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
+  const [phase, setPhase] = useState("roll-d20");
+  const [activeBossFight, setActiveBossFight] = useState(false);
 
-    const nextPlayer = () => {
-        if (players.length === 0) return;
+  const nextPlayer = () => {
+    if (players.length === 0) return;
 
-        setCurrentPlayerIndex((prev) => (prev + 1) % players.length);
-        setPhase("roll-d20");
-    };
+    setCurrentPlayerIndex((prev) => (prev + 1) % players.length);
+    setPhase("roll-d20");
+  };
 
-    return (
-        <GameContext.Provider
-            value={{
-                players,
-                setPlayers,
-                currentPlayerIndex,
-                phase,
-                setPhase,
-                nextPlayer,
-            }}
-        >
-            {children}
-        </GameContext.Provider>
-    );
+  const startBossFight = () => {
+    setActiveBossFight(true);
+    setPhase("question");
+  };
+
+  const endBossFight = () => {
+    setActiveBossFight(false);
+    setPhase("roll-d20");
+  };
+
+  return (
+    <GameContext.Provider
+      value={{
+        players,
+        setPlayers,
+        currentPlayerIndex,
+        phase,
+        setPhase,
+        nextPlayer,
+        activeBossFight,
+        setActiveBossFight,
+        startBossFight,
+        endBossFight,
+      }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
 };
