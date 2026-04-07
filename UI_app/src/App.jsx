a576163842f5e0e4c5e8ac50b91b48b63412a1ec
Victor Tønneberg <victon2003@gmail.com>
2026-04-07 20:05:40 +0200

    Setup, GameScreen, screen switching
    Legg til en state i App for å bytte mellom oppsett og spill.
    Oppdater SetupScreen til å håndtere spillere og lagre dem med setPlayers.
    Når man trykker Start, lagres spillerne og GameScreen vises.

--- UI_app/src/App.jsx
+++ UI_app/src/App.jsx
@@ -1,10 +1,17 @@
-import SetupScreen from "./components/setup/SetupScreen";
+import { useState } from "react";
 import { GameProvider } from "./context/GameProvider";
+import SetupScreen from "./components/setup/SetupScreen";
+import GameScreen from "./components/game/GameScreen";
 
 function App() {
+  const [screen, setScreen] = useState("setup");
+
   return (
     <GameProvider>
-      <SetupScreen />
+      {screen === "setup" && (
+        <SetupScreen onStartGame={() => setScreen("game")} />
+      )}
+      {screen === "game" && <GameScreen />}
     </GameProvider>
   );
 }
--- UI_app/src/components/game/GameScreen.jsx
+++ UI_app/src/components/game/GameScreen.jsx
@@ -1,1 +1,3 @@
-export default function GameScreen() {}
+export default function GameScreen() {
+  return <h1>GameScreen</h1>;
+}
