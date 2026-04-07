c5893dcd803357bffebeae74fb7d290d022adbb4
Victor Tønneberg <victon2003@gmail.com>
2026-04-07 14:48:08 +0200

    React router

--- UI_app/src/App.jsx
+++ UI_app/src/App.jsx
@@ -1,17 +1,16 @@
-import PlayerInfo from './components/game/PlayerInfo';
-import Layout from './components/Layout'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from "./components/Layout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import QuestionCard from "./components/questions/QuestionCard";
 
 function App() {
   return (
-   
-      <Layout>
-        <Routes>
-          {/* <Route path="/" element={<Home />} />  ikke det vi skal ha */}
-          {/* <Route path="/about" element={<About />} /> */}
-        </Routes>
-      </Layout>
-    
+    <Layout>
+      <Routes>
+        {/* <Route path="/" element={<Home />} />  ikke det vi skal ha */}
+        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/question" element={<QuestionCard />} />
+      </Routes>
+    </Layout>
   );
 }
-export default App
+export default App;
