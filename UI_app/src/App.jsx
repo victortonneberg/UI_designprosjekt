604a4ca6edef3b6927325a0d79d0779f65bf965f
Victor Tønneberg <victon2003@gmail.com>
2026-04-09 11:50:58 +0200

    Layout
    Implementert Layout

--- UI_app/src/App.jsx
+++ UI_app/src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from "react";
 import { GameProvider } from "./context/GameProvider";
 import SetupScreen from "./components/setup/SetupScreen";
 import GameScreen from "./components/game/GameScreen";
+import Layout from "./components/Layout";
 
 function App() {
   const [screen, setScreen] = useState("setup");
 
   return (
-    <GameProvider>
-      {screen === "setup" && (
-        <SetupScreen onStartGame={() => setScreen("game")} />
-      )}
-      {screen === "game" && <GameScreen />}
-    </GameProvider>
+    <Layout>
+      <GameProvider>
+        {screen === "setup" && (
+          <SetupScreen onStartGame={() => setScreen("game")} />
+        )}
+        {screen === "game" && <GameScreen />}
+      </GameProvider>
+    </Layout>
   );
 }
 export default App;
