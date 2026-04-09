768ac353caa21a79ad8ccb765a4bd32347fbfaf6
kristian9090 <Kristian.larsen38@gmail.com>
2026-04-09 11:56:35 +0200

    regler er oppdatert

--- UI_app/src/components/rulesButton/rulesButton.jsx
+++ UI_app/src/components/rulesButton/rulesButton.jsx
@@ -12,8 +12,24 @@ export default function RulesModal({ onClose }) {
 
         <div className="rules-text">
           <ul>
-            <li>Boss: Når man lander på eller forbi boss...</li>
-            <li>Regel 2:</li>
+            <li>
+              Først: Velger man vanskelighetsgrad og huker av hvilke kapitler
+              man vil spille med
+            </li>
+            <li>
+              Generelt: Man kaster en D-20 terning for å bestemme kategori, om
+              man svarer rett triller man deretter en d-6 terning som bestemmer
+              antall ruter man flytter.
+            </li>
+            <li>
+              Wild card: Når man lander på stjerne trekker man et kort og kan
+              bruke det når man ønsker
+            </li>
+            <li>
+              Boss: Når man lander på eller forbi boss, stopper man brikken på
+              boss, og venter til neste runde med å bekjempe boss. Man får 60
+              sekunder på å bekjempe boss. I boss-kampen triller man D-20
+            </li>
           </ul>
         </div>
       </div>
